0734304144be22fd5ee52c6373a5901512bd4631
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-10-17 15:07:58 +0900

    add multiple mocking solution

--- __tests__/repository/userRepository.test.ts
+++ __tests__/repository/userRepository.test.ts
@@ -13,7 +13,7 @@ describe("UserRepository", () => {
     });
   });
 
-  test("find() with mocked call", async () => {
+  test("find() with mocked dependency", async () => {
     const { UserApiAdaptor: MockedUserApiAdaptor } = jest.requireActual("~/infrastructure/userApiAdaptor");
 
     const mockedUserApiAdaptor: UserApiAdaptor = new MockedUserApiAdaptor();
@@ -35,5 +35,50 @@ describe("UserRepository", () => {
 
     expect(mockedUserApiAdaptor.getUser).toBeCalledTimes(1);
     expect(mockedUserApiAdaptor.getUser).toBeCalledWith(1);
+  });
+
+  test("find() with mocked static variable", async () => {
+    const { UserApiAdaptor: MockedUserApiAdaptor } = jest.requireActual("~/infrastructure/userApiAdaptor");
+    MockedUserApiAdaptor.DUMMY_DATA = {
+      id: 3,
+      name: "Static-mocked name"
+    };
+
+    const mockedUserApiAdaptor: UserApiAdaptor = new MockedUserApiAdaptor();
+
+    const repo = new UserRepository({
+      adaptor: mockedUserApiAdaptor
+    });
+
+    await expect(repo.find(1)).resolves.toStrictEqual({
+      id: 3,
+      name: "Static-mocked name",
+    });
+  });
+
+  
+  test("find() with force-mocked value", async () => {
+    const { UserApiAdaptor: MockedUserApiAdaptor } = jest.requireActual("~/infrastructure/userApiAdaptor");
+
+    const mockedUserApiAdaptor: UserApiAdaptor = new MockedUserApiAdaptor();
+    mockedUserApiAdaptor.getUser = jest.fn().mockImplementation(async () => {
+      return Promise.resolve({
+        id: 4,
+        name: "Force-mocked name"
+      });
+    });
+
+    const repo = new UserRepository({
+      adaptor: new UserApiAdaptor()
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    repo._adaptor = mockedUserApiAdaptor;
+
+    await expect(repo.find(1)).resolves.toStrictEqual({
+      id: 4,
+      name: "Force-mocked name",
+    });
   });
 });
