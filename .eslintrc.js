228002ad122cca114b3cbc9e36f1669998c82ada
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-10-17 14:07:54 +0900

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      "prettier",
-      "prettier/@typescript-eslint",
-    ],
-    "parserOptions": {
-        "ecmaVersion": 2019,
-    },
-    "env": {
-        "node": true
-    },
-  };
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+  env: {
+    node: true,
+  },
+};
