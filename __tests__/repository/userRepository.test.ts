c92618e8d4e8300fcef3680d07e2f564ffa01226
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-10-17 13:38:33 +0900

    wrap by describe

--- __tests__/repository/userRepository.test.ts
+++ __tests__/repository/userRepository.test.ts
@@ -1,13 +1,15 @@
 import { UserApiAdaptor } from "~/infrastructure/userApiAdaptor";
 import { UserRepository } from "~/repository/userRepository";
 
-test('repo.find()', async () => {
-    const repo = new UserRepository({
-        adaptor: new UserApiAdaptor()
-    });
+describe("UserRepository", () => {
+    test('find()', async () => {
+        const repo = new UserRepository({
+            adaptor: new UserApiAdaptor()
+        });
 
-    await expect(repo.find(1)).resolves.toStrictEqual({
-        id: 1,
-        name: "Real name"
+        await expect(repo.find(1)).resolves.toStrictEqual({
+            id: 1,
+            name: "Real name"
+        });
     });
 });
