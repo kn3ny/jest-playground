472ad6781fd0bcd966043755214e2e1dd0de9bb1
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-10-17 13:07:03 +0900

    change to static variable

--- src/infrastructure/userApiAdaptor.ts
+++ src/infrastructure/userApiAdaptor.ts
@@ -1,10 +1,10 @@
-const DUMMY_DATA = {
-    id: 1,
-    name: "Real name"
-};
-
 export class UserApiAdaptor {
+    private static readonly DUMMY_DATA = {
+        id: 1,
+        name: "Real name"
+    };
+
     async getUser(id: number) {
-        return Promise.resolve(DUMMY_DATA);
+        return Promise.resolve(UserApiAdaptor.DUMMY_DATA);
     }
 }
