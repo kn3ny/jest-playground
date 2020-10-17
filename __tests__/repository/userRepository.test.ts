3975673564bdb852a5c6b38e18a5547c746789d3
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-10-17 15:01:56 +0900

    mock dependencies

--- __tests__/repository/userRepository.test.ts
+++ __tests__/repository/userRepository.test.ts
@@ -2,7 +2,7 @@ import { UserApiAdaptor } from "~/infrastructure/userApiAdaptor";
 import { UserRepository } from "~/repository/userRepository";
 
 describe("UserRepository", () => {
-  test("find()", async () => {
+  test("find() with real call", async () => {
     const repo = new UserRepository({
       adaptor: new UserApiAdaptor(),
     });
@@ -11,5 +11,29 @@ describe("UserRepository", () => {
       id: 1,
       name: "Real name",
     });
+  });
+
+  test("find() with mocked call", async () => {
+    const { UserApiAdaptor: MockedUserApiAdaptor } = jest.requireActual("~/infrastructure/userApiAdaptor");
+
+    const mockedUserApiAdaptor: UserApiAdaptor = new MockedUserApiAdaptor();
+    mockedUserApiAdaptor.getUser = jest.fn().mockImplementation(async () => {
+      return Promise.resolve({
+        id: 2,
+        name: "Mocked name"
+      });
+    });
+
+    const repo = new UserRepository({
+      adaptor: mockedUserApiAdaptor
+    });
+
+    await expect(repo.find(1)).resolves.toStrictEqual({
+      id: 2,
+      name: "Mocked name",
+    });
+
+    expect(mockedUserApiAdaptor.getUser).toBeCalledTimes(1);
+    expect(mockedUserApiAdaptor.getUser).toBeCalledWith(1);
   });
 });
